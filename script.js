b0509f6ee478b4dac78b352f45b6b526edaea05a
JBxss <juanseb100@gmail.com>
2023-11-16 22:56:34 -0500

    [FIX] Protocol HTTP Block

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// Titles: https://omdbapi.com/?s=thor&page=1&apikey=fc1fef96
-// details: http://www.omdbapi.com/?i=tt3896198&apikey=fc1fef96
-
 const movieSearchBox = document.getElementById("movie-search-box");
 const searchList = document.getElementById("search-list");
 const resultGrid = document.getElementById("result-grid");
 
 //load movies from API
 async function loadMovies(searchTerm) {
-  const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+  const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=7cfa15a9`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
   // console.log(data.Search);
@@ -62,7 +59,7 @@ function loadMovieDetails() {
       searchList.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
-        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=fc1fef96`
+        `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=7cfa15a9`
       );
       const movieDetails = await result.json();
       // console.log(movieDetails);
